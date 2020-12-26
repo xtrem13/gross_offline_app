a9805bbc17f2a1a158f0174e65d8b05a946278c0
AbdumalikFront <iwillstudyingermany@gmail.com>
2020-12-26 18:05:35 +0500

    saving

--- main/controllers/user_controller.js
+++ main/controllers/user_controller.js
@@ -2,28 +2,57 @@ const { ipcMain } = require('electron')
 const window = require('electron').BrowserWindow;
 const User = require('../models/user.js');
 const axios=require('axios');
-
+const md5=require('md5');
 
 const users_controller=()=>{
 
-	const login=(arg, win)=>{
+	const login= async (arg, win)=>{
 		axios.post('http://edupark.uz/auth.php', {
 			username: arg.login,
 			password: arg.password
 		})
 		.then(function (response) {
 			let data=response.data;
-			if(data.auth==true)
-				win.webContents.send('successfull_login');
+			if(data.auth==true){
+				(async () =>{
+					let user = await User.findOne({where:{SYSTEM_ID:data.id}});
+					if(user==null){
+						await User.create({
+							NAME:data.name,
+							SYSTEM_ID:data.id
+						});
+					}
+					win.webContents.send('successfull_login',data.id);
+				})();
+			}
 		})
 		.catch(function (error) {
 			
 		});
 	}
-	
-
+	const set_local_password= async (user_id, local_password, win)=>{
+		var user=await User.findOne({where:{system_id:user_id}});
+		user.LOCAL_PASSWORD_HASH=md5(local_password);
+		await user.save();
+		win.webContents.send('local_password_saved');
+	}
+	const local_password_init = async (event, win)=>{
+		let users=await User.findAll();
+		win.webContents.send('users_loaded', users);
+	}
+	const check_local_pass=async (event, credentials, win)=>{
+		const user=await User.findOne({where:{
+				id:credentials.id,
+				LOCAL_PASSWORD_HASH:md5(credentials.password)
+			}
+		});
+		console.log(user);
+	}
 	return {
-		login:login
+		login:login,
+		set_local_password:set_local_password,
+		local_password_init:local_password_init,
+		check_local_pass:check_local_pass
 	}
 }
 module.exports=users_controller();
--- main/models/user.js
+++ main/models/user.js
@@ -7,25 +7,21 @@ const sequelize = new Sequelize({
 
 
 const User = sequelize.define('User', {
-  // Model attributes are defined here
-  user_id: {
+  SYSTEM_ID: {
     type: DataTypes.INTEGER,
     allowNull: false
   },
-  token: {
+  TOKEN: {
     type: DataTypes.STRING
     // allowNull defaults to true
   },
-  local_login:{
+  NAME:{
     type:DataTypes.STRING
   },
-  local_pass:{
+  LOCAL_PASSWORD_HASH:{
     type:DataTypes.STRING
   }
-}, {
-  // Other model options go here
 });
-
 User.sync();
 
 
